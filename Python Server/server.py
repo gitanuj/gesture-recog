70747b1276f3ccb515d0359f70a9882887478bc0
alickrxu <alickrxu@gmail.com>
2016-02-24 17:48:18 -0800

    write accelx,y,z gyrox,y,z to separate files as gesture is detected (append to file)

--- Python Server/server.py
+++ Python Server/server.py
@@ -1,6 +1,7 @@
 import socket
 import atexit
 import sys
+import csv
 
 def close_socket():
 	s.close()
@@ -12,15 +13,69 @@ s.bind((HOST, PORT))
 s.listen(0)
 
 atexit.register(close_socket)
- 
+
+                 
 while 1:
     client, addr = s.accept()
     print addr[0] + ':' + str(addr[1]) + ' connected'
 
+    accel_x = []
+    accel_y = []
+    accel_z = []
+    accel = []
+    gyro_x = []
+    gyro_y = []
+    gyro_z = []
+    gyro = []
+
     while 1:
-    	data = client.recv(1024)
-    	print data
-    	if not data:
-    		client.close()
-    		print addr[0] + ':' + str(addr[1]) + ' connection closed'
+        data = client.recv(1024)
+        values = data.split()
+
+        if data: #there is data, add it
+            accel_x.append(values[0])
+            accel_y.append(values[1])
+            accel_z.append(values[2])
+
+            gyro_x.append(values[3])
+            gyro_y.append(values[4])
+            gyro_z.append(values[5])
+            
+            print data
+
+        else: #no more data, write to file
+            client.close()
+            print addr[0] + ':' + str(addr[1]) + ' connection closed'
+            print accel_x
+
+            with open('accel_x.csv', 'a') as accelxfile:
+                writer = csv.writer(accelxfile)
+                for x_data in accel_x:
+                    writer.writerow([x_data])
+
+            with open('accel_y.csv', 'a') as accelyfile:
+                writer = csv.writer(accelyfile)
+                for y_data in accel_y:
+                    writer.writerow([y_data])
+
+            with open('accel_z.csv', 'a') as accelzfile:
+                writer = csv.writer(accelzfile)
+                for z_data in accel_z:
+                    writer.writerow([z_data])
+
+            with open('gyro_x.csv', 'a') as gfile:
+                writer = csv.writer(gfile)
+                for x_data in gyro_x:
+                    writer.writerow([x_data])
+
+            with open('gyro_y.csv', 'a') as gfile:
+                writer = csv.writer(gfile)
+                for y_data in gyro_y:
+                    writer.writerow([y_data])
+
+            with open('gyro_z.csv', 'a') as gfile:
+                writer = csv.writer(gfile)
+                for z_data in gyro_z:
+                    writer.writerow([z_data])
+
     		break
