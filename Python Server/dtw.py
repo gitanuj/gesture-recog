ed90811d92b971fa403f9cc09be47bfd61b7dad7
alickrxu <alickrxu@gmail.com>
2016-02-27 15:44:19 -0800

    basic 1-nn classifier for accel x values

--- Python Server/dtw.py
+++ Python Server/dtw.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys
+import os
 from math import sqrt
 
 def DTWDistance(s1, s2,w):
@@ -19,7 +20,48 @@ def DTWDistance(s1, s2,w):
         
     return sqrt(DTW[len(s1)-1, len(s2)-1])
 
-x = np.genfromtxt(str(sys.argv[1])).reshape(-1, 1)
-y = np.genfromtxt(str(sys.argv[2])).reshape(-1, 1)
+# start script
+testgesture1 = np.genfromtxt(str(sys.argv[1])).reshape(-1, 1)
+#testgesture2 = np.genfromtxt(str(sys.argv[2])).reshape(-1, 1)
 
-print DTWDistance(x, y, 25)
+dataPath = ''
+dataDirs = []
+
+# first get the directory where data lives, and all of the subdirectories of data
+currDir = os.path.dirname(os.path.realpath(__file__))
+for dirName, subDirList, fileList in os.walk(currDir):
+    if dirName.endswith('data'):
+        print 'whoa'
+        dataPath = dirName
+        dataDirs = subDirList
+
+print dataPath
+print dataDirs
+
+minDTWDistance = sys.float_info.max
+theClass = ''
+
+# now calculate DTWDistance for each gesture
+for dataDir in dataDirs:
+    currPath = dataPath + ("/" + dataDir + "/")
+    print currPath
+    axTrain = None
+
+    try:
+        axTrain = open(currPath + "ax.txt", "r")
+    except:
+        print 'couldnt open file'
+
+    print axTrain
+
+    arrays = [np.array(map(float, line.split())) for line in axTrain]
+
+    for array in arrays:
+        currDTW = DTWDistance(testgesture1, array, 25)
+        if currDTW < minDTWDistance:
+            print currDTW
+            minDTWDistance = currDTW
+            theClass = dataDir
+
+print theClass
+print minDTWDistance
