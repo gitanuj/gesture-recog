e9310824d462fef02764247fe9a2abf7a672ca2c
Tanuj Mittal <tanuj.183@gmail.com>
2016-02-28 16:33:50 -0800

    Modify parse_data to be used as a function

--- Python Server/parse_data.py
+++ Python Server/parse_data.py
@@ -1,29 +1,41 @@
 import sys
+import os
 
-with open(str(sys.argv[1])) as f:
-    data = f.readlines()
+def parse(data):
+	parsed_data = [''] * 6
 
-files = []
-files.append(open('ax.txt', 'w'))
-files.append(open('ay.txt', 'w'))
-files.append(open('az.txt', 'w'))
-files.append(open('gx.txt', 'w'))
-files.append(open('gy.txt', 'w'))
-files.append(open('gz.txt', 'w'))
-
-for line in data:
-	if not line.strip():
-		continue
-		
-	for value_set in line.split(','):
-		if not value_set.strip():
+	for line in data:
+		if not line.strip():
 			continue
+		
+		for value_set in line.split(','):
+			if not value_set.strip():
+				continue
 
-		values = value_set.split()
-		for i in range(0, 6):
-			files[i].write(values[i] + ' ')
-	for file in files:
-		file.write('\n')
+			values = value_set.split()
+			for i in range(0, 6):
+				parsed_data[i] += values[i] + ' '
+	
+		for p in parsed_data:
+			p += '\n'
 
-for file in files:
-	file.close()
+	return parsed_data
+
+if __name__ == '__main__':
+	with open(str(sys.argv[1])) as f:
+		data = f.readlines()
+
+	dirname = os.path.dirname(os.path.realpath(sys.argv[1]))
+	parsed_data = parse(data)
+
+	files = [None] * 6
+	files[0] = open(dirname + '/ax.txt', 'w')
+	files[1] = open(dirname + '/ay.txt', 'w')
+	files[2] = open(dirname + '/az.txt', 'w')
+	files[3] = open(dirname + '/gx.txt', 'w')
+	files[4] = open(dirname + '/gy.txt', 'w')
+	files[5] = open(dirname + '/gz.txt', 'w')
+
+	for i, p in enumerate(parsed_data):
+		files[i].write(p)
+		files[i].close()
